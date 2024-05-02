b0b73ed164fa8b84ebd64e59edac98bf3d8d5d39
Lethusang <kotjomelalethusang@gmail.com>
2024-05-02 08:29:27 +0200

    Crud methods

--- src/main/java/org/openlmis/dispensing/web/PrescriptionController.java
+++ src/main/java/org/openlmis/dispensing/web/PrescriptionController.java
@@ -15,45 +15,61 @@
 
 package org.openlmis.dispensing.web;
 
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.util.List;
 import java.util.UUID;
 import org.openlmis.dispensing.dto.prescription.PrescriptionDto;
 import org.openlmis.dispensing.service.prescription.PrescriptionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.profiler.Profiler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/**
+ * Controller used to perform CRUD operations on point of delivery event.
+ */
+@Controller
 @RequestMapping("/api/prescription")
 public class PrescriptionController extends BaseController {
   public static final String ID_PATH_VARIABLE = "/{id}";
-
-  static {
-    org.slf4j.LoggerFactory.getLogger(PrescriptionController.class);
-  }
+  private static final Logger LOGGER = LoggerFactory.getLogger(PrescriptionController.class);
 
   @Autowired
-  private final PrescriptionService prescriptionService;
+  private PrescriptionService prescriptionService;
 
-  public PrescriptionController(PrescriptionService prescriptionService) {
-    this.prescriptionService = prescriptionService;
-  }
 
-  @PostMapping
+  /**
+   * Create prescription.
+   *
+   * @param prescriptionDto a prescription dto bound to request body.
+   * @return created prescription's ID.
+   */
+  @Transactional
+  @RequestMapping(method = POST)
   public ResponseEntity<UUID> createPrescription(@org.springframework.web.bind.annotation.RequestBody PrescriptionDto prescriptionDto) {
-    UUID id = prescriptionService.createPrescription(prescriptionDto);
-    return new ResponseEntity<>(id, HttpStatus.CREATED);
+    LOGGER.debug("Try to create a prescription");
+    Profiler profiler = getProfiler("CREATE_PRESCRIPTION", prescriptionDto);
+
+    profiler.start("PROCESS");
+    UUID createdPrescriptionId = prescriptionService.createPrescription(prescriptionDto);
+
+    profiler.start("CREATE_RESPONSE");
+    ResponseEntity<UUID> response = new ResponseEntity<>(createdPrescriptionId, CREATED);
+
+    return stopProfiler(profiler, response);
   }
 
   /**
@@ -79,8 +95,9 @@ public class PrescriptionController extends BaseController {
    * @return Updated Prescription dto.
    */
   @Transactional
-  @PutMapping("/{id}")
+  @PutMapping(ID_PATH_VARIABLE)
   @ResponseStatus(OK)
+  @ResponseBody
   public ResponseEntity<PrescriptionDto> updatePrescription(@PathVariable UUID id, @RequestBody PrescriptionDto dto) {
     PrescriptionDto updatedPrescription = prescriptionService.updatePrescription(id, dto);
     return new ResponseEntity<>(updatedPrescription, OK);
