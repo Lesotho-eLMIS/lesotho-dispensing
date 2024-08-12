568db19cdd0dfaaee7af0de9c08b8c4fd6dfecf3
lmphatsi <la.mphatsi@gmail.com>
2024-08-12 15:43:38 +0200

    allow partial matches on patient names to be returned

--- src/main/java/org/openlmis/dispensing/util/PatientSpecifications.java
+++ src/main/java/org/openlmis/dispensing/util/PatientSpecifications.java
@@ -18,7 +18,6 @@ package org.openlmis.dispensing.util;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
 import javax.persistence.criteria.Predicate;
 import org.openlmis.dispensing.domain.patient.Patient;
 import org.springframework.data.jpa.domain.Specification;
@@ -28,65 +27,74 @@ public class PatientSpecifications {
   public static final String PERSON_FIELD = "person";
   
   /**
-   * Specification.
-   *
-   * @return Specification.
-   */
-
+  * Specification for matching patient number.
+  *
+  * @return Specification.
+  */
   public static Specification<Patient> hasPatientNumber(String patientNumber) {
     return (root, query, cb) -> cb.equal(root.get("patientNumber"), patientNumber);
   }
 
   /**
-   * Specification.
-   *
-   * @return Specification
-   */
+  * Specification for matching partial first name.
+  *
+  * @return Specification.
+  */
   public static Specification<Patient> hasFirstName(String firstName) {
-    return (root, query, cb) -> cb.equal(root.get(PERSON_FIELD).get("firstName"), firstName);
+    return (root, query, cb) -> {
+      if (firstName != null) {
+        return cb.like(cb.lower(root.get(PERSON_FIELD).get("firstName")), "%" + firstName.toLowerCase() + "%");
+      }
+      return cb.conjunction();
+    };
   }
 
   /**
-   * Specification.
-   *
-   * @return specification.
-   */
+  * Specification for matching partial last name.
+  *
+  * @return Specification.
+  */
   public static Specification<Patient> hasLastName(String lastName) {
-    return (root, query, cb) -> cb.equal(root.get(PERSON_FIELD).get("lastName"), lastName);
+    return (root, query, cb) -> {
+      if (lastName != null) {
+        return cb.like(cb.lower(root.get(PERSON_FIELD).get("lastName")), "%" + lastName.toLowerCase() + "%");
+      }
+      return cb.conjunction();
+    };
   }
 
   /**
-   * Specification.
-   *
-   * @return specification.
-   */
+  * Specification for matching facility code.
+  *
+  * @return Specification.
+  */
   public static Specification<Patient> hasFacilityCode(UUID facilityId) {
     return (root, query, cb) -> cb.equal(root.get("facilityId"), facilityId);
   }
 
   /**
-   * Specification.
-   *
-   * @return specification.
-   */
+  * Specification for matching national ID.
+  *
+  * @return Specification.
+  */
   public static Specification<Patient> hasNationalId(String nationalId) {
     return (root, query, cb) -> cb.equal(root.get(PERSON_FIELD).get("nationalId"), nationalId);
   }
 
   /**
-   * Specification.
-   *
-   * @return Specification.
-   */
+  * Specification for matching date of birth.
+  *
+  * @return Specification.
+  */
   public static Specification<Patient> hasDateOfBirth(String dateOfBirth) {
     return (root, query, cb) -> cb.equal(root.get(PERSON_FIELD).get("dateOfBirth"), dateOfBirth);
   }
 
   /**
-   * Specification.
-   *
-   * @return Specification.
-   */
+  * Specification for combining multiple search criteria.
+  *
+  * @return Specification.
+  */
   public static Specification<Patient> bySearchCriteria(String patientNumber, String firstName, String lastName, String dateOfBirth, UUID facilityId, String nationalId) {
     return (root, query, cb) -> {
       List<Predicate> predicates = new ArrayList<>();
@@ -94,10 +102,10 @@ public class PatientSpecifications {
         predicates.add(cb.equal(root.get("patientNumber"), patientNumber));
       }
       if (firstName != null) {
-        predicates.add(cb.equal(root.get(PERSON_FIELD).get("firstName"), firstName));
+        predicates.add(cb.like(cb.lower(root.get(PERSON_FIELD).get("firstName")), "%" + firstName.toLowerCase() + "%"));
       }
       if (lastName != null) {
-        predicates.add(cb.equal(root.get(PERSON_FIELD).get("lastName"), lastName));
+        predicates.add(cb.like(cb.lower(root.get(PERSON_FIELD).get("lastName")), "%" + lastName.toLowerCase() + "%"));
       }
       if (dateOfBirth != null) {
         predicates.add(cb.equal(root.get(PERSON_FIELD).get("dateOfBirth"), dateOfBirth));
@@ -112,3 +120,4 @@ public class PatientSpecifications {
     };
   }
 }
+ 
